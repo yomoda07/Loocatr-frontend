9abcc2d837e765606ce12162df229fa5c6bb407f
yomoda07 <kanji.yy@gmail.com>
2017-10-23 19:12:14 -0700

    Link react-native-fetch-bolb

--- LoocatrFrontend/android/app/src/main/java/com/loocatrfrontend/MainApplication.java
+++ LoocatrFrontend/android/app/src/main/java/com/loocatrfrontend/MainApplication.java
@@ -3,6 +3,7 @@ package com.loocatrfrontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new PickerPackage(),
             new VectorIconsPackage(),
             new MapsPackage()
