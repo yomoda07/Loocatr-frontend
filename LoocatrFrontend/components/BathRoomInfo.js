4b4ccaf3530d12d3b166481ae2eb1fca5455a206
Anson <leungans@gmail.com>
2017-10-25 09:48:53 -0700

    Update BathRoomInfo.js

--- LoocatrFrontend/components/BathRoomInfo.js
+++ LoocatrFrontend/components/BathRoomInfo.js
@@ -72,7 +72,7 @@ export default class BathRoomInfo extends Component {
       this.setState({ uid: JSON.parse(value).uid });
     }).done(() => {
       if (!this.state.uid) {
-        this.setState({ uid: anonymous })
+        this.setState({ uid: "anonymous" })
       }
     });
   }
